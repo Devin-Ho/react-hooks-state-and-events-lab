817ade9ce335259dd7a8f7ab157b38e9851c2f85
Devin Ho <djxho36@gmail.com>
2022-08-01 17:47:53 -0400

    commit tree e497136515a6613e05cd888034facdbc9b30ac02

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  let [isOn, setIsOn] = useState (false)
+  function handleClick (event) {
+    setIsOn(!isOn)
+    // if (setIsOn((isOn) = !isOn)) {
+    //   event.target.style.removeProperty('text-decoration');
+    // } else {
+    //   event.target.style.setProperty('text-decoration', 'line-through')
+    // }
+  }
+
+  const color = isOn ? "purple" : "yellow";
   return (
-    <li className="">
+    <li className={isOn ? 'in-cart': ""} >
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button style = {{background: color}} className="add" onClick = {handleClick}> {isOn ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
